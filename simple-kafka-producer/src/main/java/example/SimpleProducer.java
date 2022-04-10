23368eadbc1e1f6198dca5dcf7095cf9b7396fd8
joy <ionateous@gmail.com>
2022-04-10 16:30:15 +0900

    update

---
+++ simple-kafka-producer/src/main/java/example/SimpleProducer.java
@@ -0,0 +1,35 @@
+package example;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+import java.util.Properties;
+
+public class SimpleProducer {
+    private final static Logger logger = LoggerFactory.getLogger(SimpleProducer.class);
+    private final static String TOPIC_NAME = "test";
+    private final static String BOOTSTRAP_SERVERS = "my-kafka:9092";
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        KafkaProducer<String, String> producer = new KafkaProducer<>(props);
+
+        String message = "Hello";
+        ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, message);
+
+        producer.send(record);
+        
+        logger.info("{}", record);
+
+        producer.flush();
+        producer.close();
+    }
+}
